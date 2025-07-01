47863922a7b2d0c7da4114170d3a1706b01a8bca
胡凌寒 <hulh01@yuanfudao.com>
2025-07-01 12:19:44 +0800

    fixup

--- init.sh
+++ init.sh
@@ -9,3 +9,12 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 
 # copy zsh config
 ln -sf $dotfiles_dir/.zshrc $HOME/.zshrc
+
+# copy bin file to user/local/bin
+ln -sf $dotfiles_dir/bin/dc /usr/local/bin/dc
+ln -sf $dotfiles_dir/bin/pull /usr/local/bin/pull
+ln -sf $dotfiles_dir/bin/push /usr/local/bin/push
+
+chmod +x /usr/local/bin/dc
+chmod +x /usr/local/bin/pull 
+chmod +x /usr/local/bin/push
