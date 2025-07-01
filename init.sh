aedb953602f57a27f12bd19e6ec231755c35a27a
胡凌寒 <hulh01@yuanfudao.com>
2025-07-01 11:50:30 +0800

    init zsh

---
+++ init.sh
@@ -0,0 +1,11 @@
+# install.sh
+#!/usr/bin/env bash
+set -eu
+
+dotfiles_dir="$HOME"/dotfiles
+
+# install omz
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# copy zsh config
+ln -sf $dotfiles_dir/.zshrc $HOME/.zshrc
